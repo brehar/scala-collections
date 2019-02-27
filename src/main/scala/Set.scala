b1ea1180121c4b683ffaf34f8658a0d6d7856c35
Brett Hartman <brett.hartman@mac.com>
2019-02-27 09:36:33 -0800

    Generalize methods

--- src/main/scala/Set.scala
+++ src/main/scala/Set.scala
@@ -1,7 +1,19 @@
 sealed trait Set[E] extends (E => Boolean) {
   import Set.{ Cons, Empty, empty }
 
-  final override def apply(input: E): Boolean = fold(false)(_ || _ == input)
+  final override def apply(input: E): Boolean = contains(input)
+
+  final def doesNotContain(input: E): Boolean = !contains(input)
+
+  final def contains(input: E): Boolean = exists(_ == input)
+
+  final def doesNotExist(predicate: E => Boolean): Boolean = !exists(predicate)
+
+  final def exists(predicate: E => Boolean): Boolean = fold(false)(_ || predicate(_))
+
+  final def notForall(predicate: E => Boolean): Boolean = !forall(predicate)
+
+  final def forall(predicate: E => Boolean): Boolean = fold(true)(_ && predicate(_))
 
   final def add(input: E): Set[E] = fold(Cons(input, empty)) { (acc, current) =>
     if (current == input) acc
@@ -15,8 +27,10 @@ sealed trait Set[E] extends (E => Boolean) {
 
   final def union(that: Set[E]): Set[E] = fold(that)(_ add _)
 
-  final def intersection(that: Set[E]): Set[E] = fold(empty[E]) { (acc, current) =>
-    if (that(current)) acc.add(current)
+  final def intersection(that: Set[E]): Set[E] = filter(that)
+
+  final def filter(predicate: E => Boolean): Set[E] = fold(empty[E]) { (acc, current) =>
+    if (predicate(current)) acc.add(current)
     else acc
   }
 
@@ -25,7 +39,7 @@ sealed trait Set[E] extends (E => Boolean) {
     else acc.add(current)
   }
 
-  final def isSubsetOf(that: Set[E]): Boolean = fold(true)(_ && that(_))
+  final def isSubsetOf(that: Set[E]): Boolean = forall(that)
 
   final def isSupersetOf(that: Set[E]): Boolean = that.isSubsetOf(this)
 
@@ -36,6 +50,17 @@ sealed trait Set[E] extends (E => Boolean) {
 
   final override def hashCode: Int = fold(42)(_ + _.hashCode())
 
+  final override def toString: String =
+    if (isEmpty) "{}"
+    else {
+      val otherElementsSplitByCommaSpace = otherElementsOrThrowException.fold("") {
+        (acc, current) =>
+          s"$acc, $current"
+      }
+
+      s"{ $elementOrThrowException$otherElementsSplitByCommaSpace }"
+    }
+
   final def size: Int = fold(0) { (acc, _) =>
     acc + 1
   }
@@ -44,22 +69,16 @@ sealed trait Set[E] extends (E => Boolean) {
 
   final def nonEmpty: Boolean = !isEmpty
 
-  def isSingleton: Boolean =
-    if (isEmpty) false
-    else this.asInstanceOf[Cons[E]].otherElements.isEmpty
+  def isSingleton: Boolean = nonEmpty && otherElementsOrThrowException.isEmpty
 
   def sample: Option[E] =
     if (isEmpty) None
-    else Some(this.asInstanceOf[Cons[E]].element)
+    else Some(elementOrThrowException)
 
   @scala.annotation.tailrec
   final def fold[R](seed: R)(function: (R, E) => R): R = {
     if (isEmpty) seed
-    else {
-      val cons = this.asInstanceOf[Cons[E]]
-
-      cons.otherElements.fold(function(seed, cons.element))(function)
-    }
+    else otherElementsOrThrowException.fold(function(seed, elementOrThrowException))(function)
   }
 
   final def foreach[R](function: E => R): Unit = fold(()) { (_, current) =>
@@ -70,6 +89,12 @@ sealed trait Set[E] extends (E => Boolean) {
 
   final def flatMap[R](function: E => Set[R]): Set[R] = fold(empty[R]) { (acc, current) =>
     function(current).fold(acc)(_ add _)
+  }
+
+  private[this] lazy val (elementOrThrowException, otherElementsOrThrowException) = {
+    val cons = this.asInstanceOf[Cons[E]]
+
+    cons.element -> cons.otherElements
   }
 }
 
