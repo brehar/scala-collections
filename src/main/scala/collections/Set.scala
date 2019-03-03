368e3032a49c0db8618d50504be87269d0ce8055
Brett Hartman <brett.hartman@mac.com>
2019-03-02 17:26:42 -0800

    Refactor Set to remove lazy val

--- src/main/scala/collections/Set.scala
+++ src/main/scala/collections/Set.scala
@@ -1,16 +1,16 @@
 package collections
 
-sealed trait Set[+E] extends FoldableFactory[E, Set] {
-  import Set.{ Cons, empty }
+sealed abstract class Set[+E] extends FoldableFactory[E, Set] {
+  import Set.{ Cons, Empty, empty }
 
   final protected def factory: Factory[Set] = Set
 
   final def apply[S >: E](input: S): Boolean = contains(input)
 
   @scala.annotation.tailrec
-  final def fold[R](seed: R)(function: (R, E) => R): R = {
-    if (isEmpty) seed
-    else otherElementsOrThrowException.fold(function(seed, elementOrThrowException))(function)
+  final def fold[R](seed: R)(function: (R, E) => R): R = this match {
+    case _: Empty.type => seed
+    case cons: Cons[E] => cons.otherElements.fold(function(seed, cons.element))(function)
   }
 
   final def add[S >: E](input: S): Set[S] = fold(Cons(input, empty)) { (acc, current) =>
@@ -43,36 +43,29 @@ sealed trait Set[+E] extends FoldableFactory[E, Set] {
 
   final override def hashCode: Int = fold(42)(_ + _.hashCode())
 
-  final override def toString: String =
-    if (isEmpty) "{}"
-    else {
-      val otherElementsSplitByCommaSpace = otherElementsOrThrowException.fold("") {
-        (acc, current) =>
-          s"$acc, $current"
-      }
-
-      s"{ $elementOrThrowException$otherElementsSplitByCommaSpace }"
-    }
-
   final def isEmpty: Boolean = this eq empty
 
   final def nonEmpty: Boolean = !isEmpty
 
-  def isSingleton: Boolean = nonEmpty && otherElementsOrThrowException.isEmpty
+  def isSingleton: Boolean
 
-  def sample: Option[E] =
-    if (isEmpty) None
-    else Some(elementOrThrowException)
-
-  private[this] lazy val (elementOrThrowException, otherElementsOrThrowException) = {
-    val cons = this.asInstanceOf[Cons[E]]
-
-    cons.element -> cons.otherElements
-  }
+  def sample: Option[E]
 }
 
 object Set extends Factory[Set] {
-  private final case class Cons[E](element: E, otherElements: Set[E]) extends Set[E]
+  private final case class Cons[E](element: E, otherElements: Set[E]) extends Set[E] {
+    def isSingleton: Boolean = otherElements.isEmpty
+
+    def sample: Option[E] = Some(element)
+
+    override def toString: String =
+      "{ " + element + otherElementsSplitByCommaSpace(otherElements) + " }"
+
+    private[this] def otherElementsSplitByCommaSpace(input: Set[E]): String = input.fold("") {
+      (acc, current) =>
+        s"$acc, $current"
+    }
+  }
 
   private object Cons {
     // $COVERAGE-OFF$
@@ -81,6 +74,12 @@ object Set extends Factory[Set] {
   }
 
   private object Empty extends Set[Nothing] {
+    final def isSingleton: Boolean = false
+
+    final def sample: Option[Nothing] = None
+
+    final override def toString: String = "{}"
+
     // $COVERAGE-OFF$
     private[this] def unapply(any: Any): Option[(String, Any)] = patternMatchingNotSupported
     // $COVERAGE-ON$
