c29d9098e758fda00d7fa8b0a570e66d0d256027
Brett Hartman <brett.hartman@mac.com>
2019-03-04 06:32:37 -0800

    Implement balanced adding with red-black trees

--- src/main/scala/collections/Set.scala
+++ src/main/scala/collections/Set.scala
@@ -1,7 +1,8 @@
 package collections
 
 sealed abstract class Set[+E] extends FoldableFactory[E, Set] {
-  import Set.{ Cons, Empty, empty }
+  import Set.Color.Red
+  import Set.{ Color, Cons, Empty, Okasaki, empty }
 
   final protected def factory: Factory[Set] = Set
 
@@ -10,7 +11,7 @@ sealed abstract class Set[+E] extends FoldableFactory[E, Set] {
   @scala.annotation.tailrec
   final override def contains[S >: E](input: S): Boolean = this match {
     case Empty() => false
-    case Cons(left, element, right) =>
+    case Cons(_, left, element, right) =>
       if (input == element) true
       else if (input.hashCode() <= element.hashCode()) left.contains(input)
       else right.contains(input)
@@ -18,27 +19,35 @@ sealed abstract class Set[+E] extends FoldableFactory[E, Set] {
 
   final def fold[R](seed: R)(function: (R, E) => R): R = this match {
     case Empty() => seed
-    case Cons(left, element, right) =>
+    case Cons(_, left, element, right) =>
       val currentResult = function(seed, element)
       val rightResult = right.fold(currentResult)(function)
 
       left.fold(rightResult)(function)
   }
 
-  final def add[S >: E](input: S): Set[S] = this match {
-    case Empty() => Cons(empty, input, empty)
-    case Cons(left, element, right) =>
-      if (input == element) this
-      else if (input.hashCode() <= element.hashCode()) Cons(left.add(input), element, right)
-      else Cons(left, element, right.add(input))
+  final def add[S >: E](input: S): Set[S] = {
+    val okasaki = new Okasaki[S]
+
+    def ins(set: Set[S]): Set[S] = set match {
+      case Empty() => Cons(Red, empty, input, empty)
+      case Cons(color, left, element, right) =>
+        if (input == element) set
+        else if (input.hashCode() <= element.hashCode())
+          okasaki.balance(Cons(color, ins(left), element, right))
+        else okasaki.balance(Cons(color, left, element, ins(right)))
+    }
+
+    okasaki.makeBlack(ins(this))
   }
 
   final def remove[S >: E](input: S): Set[S] = this match {
     case Empty() => empty
-    case Cons(left, element, right) =>
+    case Cons(color, left, element, right) =>
       if (input == element) left.union(right)
-      else if (input.hashCode() <= element.hashCode()) Cons(left.remove(input), element, right)
-      else Cons(left, element, right.remove(input))
+      else if (input.hashCode() <= element.hashCode())
+        Cons(color, left.remove(input), element, right)
+      else Cons(color, left, element, right.remove(input))
   }
 
   final def union[S >: E](that: Set[S]): Set[S] = fold(that)(_ add _)
@@ -82,8 +91,8 @@ sealed abstract class Set[+E] extends FoldableFactory[E, Set] {
 
     def loop(prefix: String, isLeft: Boolean, isFirst: Boolean, set: Set[E]): String = set match {
       case Empty() => ""
-      case Cons(left, element, right) =>
-        prefix + leftOrRight(isLeft, isFirst) + element + "\n" + loop(
+      case Cons(color, left, element, right) =>
+        prefix + leftOrRight(isLeft, isFirst) + color.paint(element) + "\n" + loop(
           prefix + leftOrRightParent(isLeft, isFirst),
           isLeft = false,
           isFirst = false,
@@ -96,10 +105,13 @@ sealed abstract class Set[+E] extends FoldableFactory[E, Set] {
 
     loop("", isLeft = true, isFirst = true, this)
   }
+
+  protected def color: Color
 }
 
 object Set extends Factory[Set] {
-  private final case class Cons[+E](left: Set[E], element: E, right: Set[E]) extends Set[E] {
+  private final case class Cons[+E](color: Color, left: Set[E], element: E, right: Set[E])
+    extends Set[E] {
     def isSingleton: Boolean = left.isEmpty && right.isEmpty
 
     def sample: Option[E] = Some(element)
@@ -120,9 +132,69 @@ object Set extends Factory[Set] {
     final def sample: Option[Nothing] = None
 
     final override def toString: String = "{}"
+
+    final def color: Color = Color.Black
   }
 
   final def empty: Set[Nothing] = Empty
 
+  private[Set] sealed abstract class Color {
+    def paint(element: Any): String
+  }
+
+  private[Set] object Color {
+    case object Red extends Color {
+      final def paint(element: Any): String = Console.RED + element + Console.RESET
+    }
+
+    case object Black extends Color {
+      final def paint(element: Any): String = Console.BLUE + element + Console.RESET
+    }
+  }
+
+  private[Set] class Okasaki[S] {
+    import Set.Color.{ Black, Red }
+
+    private def solution[E](a: Set[E], b: Set[E], c: Set[E], d: Set[E], x: E, y: E, z: E): Set[E] =
+      Cons(Red, Cons(Black, a, x, b), y, Cons(Black, c, z, d))
+
+    def makeBlack(set: Set[S]): Set[S] = set match {
+      case Empty() => set
+      case Cons(_, left, element, right) => Cons(Black, left, element, right)
+    }
+
+    private type Case = PartialFunction[Set[S], Set[S]]
+
+    private lazy val top: Case = {
+      case Cons(Black, Cons(Red, a, x, Cons(Red, b, y, c)), z, d) => solution(a, b, c, d, x, y, z)
+    }
+
+    private lazy val right: Case = {
+      case Cons(Black, a, x, Cons(Red, b, y, Cons(Red, c, z, d))) => solution(a, b, c, d, x, y, z)
+    }
+
+    private lazy val bottom: Case = {
+      case Cons(Black, a, x, Cons(Red, Cons(Red, b, y, c), z, d)) => solution(a, b, c, d, x, y, z)
+    }
+
+    private lazy val left: Case = {
+      case Cons(Black, Cons(Red, Cons(Red, a, x, b), y, c), z, d) => solution(a, b, c, d, x, y, z)
+    }
+
+    private lazy val center: Case = {
+      case balanced => balanced
+    }
+
+    lazy val balance: Case = top orElse right orElse bottom orElse left orElse center
+  }
+
   implicit def setCanBeUsedAsFunction1[E](set: Set[E]): E => Boolean = set.apply
+}
+
+object Main extends App {
+  println("-" * 50)
+
+  println(Set(0, 1 to 19: _*).rendered)
+
+  println("-" * 50)
 }
