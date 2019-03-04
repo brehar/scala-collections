dcfc3428286da912c8379090d7c6fbf2e56c62a7
Brett Hartman <brett.hartman@mac.com>
2019-03-04 13:34:00 -0800

    Implement Stack data structure

---
+++ src/main/scala/collections/Stack.scala
@@ -0,0 +1,24 @@
+package collections
+
+sealed abstract class Stack[+E] {
+  import Stack.{ Cons, Empty, empty }
+
+  final def `onTopOf:`[S >: E](input: S): Stack[S] = push(input)
+
+  final def push[S >: E](input: S): Stack[S] = Cons(input, this)
+
+  final lazy val (peek, pop) = popElement
+
+  final def popElement: (Option[E], Stack[E]) = this match {
+    case Empty => None -> empty
+    case Cons(element, otherElements) => Some(element) -> otherElements
+  }
+}
+
+object Stack {
+  final case class Cons[+E](element: E, otherElements: Stack[E]) extends Stack[E]
+
+  final case object Empty extends Stack[Nothing]
+
+  def empty: Stack[Nothing] = Empty
+}
